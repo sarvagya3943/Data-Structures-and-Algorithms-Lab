e091cdd1629de38c9893173bf992e2652f7515d3
sarvagya <sarvagyaagarwal01@gmail.com>
2018-04-14 06:20:16 +0530

    Added some code for lab 11

--- lab11/code.c
+++ lab11/code.c
@@ -8,16 +8,17 @@
 typedef struct Edge Edge;
 typedef struct EdgeList EdgeList;
 typedef struct AdjList AdjList;
+typedef struct list list ;
 
 struct Edge{
 	int u,v;
 };
 
 Edge makeEdge(int u,int v) {
-	Edge x=(Edge)malloc(sizeof(Edge));
-	x->u=u;
-	x->v=v;
-	return *x;
+	Edge x ;
+	x.u=u;
+	x.v=v;
+	return x ;
 }
 
 struct EdgeList{
@@ -36,7 +37,7 @@ EdgeList *createEdgeList(char *name) {
 	}
 
 	//printf("%d\n",sz);
-	EdgeList e = (EdgeList)malloc(sizeof(EdgeList));
+	EdgeList *e = (EdgeList*)malloc(sizeof(EdgeList));
 	e->sz=sz;
 	e->arr=(Edge*)malloc(sizeof(Edge)*sz);
 	
@@ -83,8 +84,100 @@ void fillAdjList(EdgeList *e,AdjList *adj) {
 }
 
 int indeg[MAX_VERTICES],outdeg[MAX_VERTICES];
+int inserted[MAX_VERTICES] ; 
 
+typedef struct node node ;
+struct node {
+    int val ; node *next ; 
+};
+struct list {
+    node *head , *tail ;
+    int size ; 
+};
 
+node* newNode(int temp) {
+    node* res = (node*) malloc(sizeof(node)) ;
+    res->val = temp ; 
+    res->next = NULL ; return res ;
+}
+
+list* insert(list* l , node* n) {
+    if(l->size == 0) {
+        l->size = 1 ; 
+        l->head = l->tail = n ; 
+        return l ; 
+    }
+    l->tail->next = n ; 
+    l->tail = n; 
+    l->size++ ; 
+    return l; 
+}
+
+// toposort with edge list representation 
+// naive way because heap kon implement karega be 
+list* toposort1(EdgeList* e) {
+    list* res = (list*) malloc(sizeof(list)) ; 
+    list->head = list->tail = NULL ; 
+    list->size = 0 ; 
+    int esize = e->sz ;
+    int i ; 
+    memset(inserted , 0 , sizeof(inserted)) ; 
+    while(1) {
+        int found = 0 ; 
+        int temp = 0 ; 
+        for(i = 0 ; i < esize ; ++i) {
+            int u = e->arr[i].u ; 
+            int v = e->arr[i].v ;
+            if(indeg[u] == 0 && !inserted[u]) {
+                temp = u ; found = 1 ; break ;  
+            }
+            // this can happen , since we dont delete edges 
+            // particularly when u is already in the toposort
+            if(indeg[v] == 0 && !inserted[v]) {
+                temp = v ; found = 1 ; break ;  
+            }
+        }
+        if(!found) break ;
+        inserted[temp] = 1 ; 
+        node* new_node = newNode(temp) ; 
+        res = insert(res , new_node) ;
+        for(i = 0 ; i < esize ; ++i) {
+            int u = e->arr[i].u , v = e->arr[i].v ; 
+            if(u == temp) {
+                --indeg[v] ;  
+            }
+        }
+    }
+    return res ; 
+}
+
+// toposort with adj list implementation 
+list* toposort2(AdjList* adj) {
+    list* res = (list*) malloc(sizeof(list)) ; 
+    list->head = list->tail = NULL ; 
+    list->size = 0 ; 
+    memset(inserted , 0 , sizeof(inserted)) ; 
+    int i; 
+    while(1) {
+        int found = 0 ; 
+        for(i = 0 ; i < MAX_VERTICES ; ++i) {
+            if(indeg[i] == 0 && !inserted[i]) {
+                found = 1 ; 
+                res = insert(res , newNode(i)) ;
+                inserted[i] = 1 ; 
+                AdjList* l = adj + i ; 
+                int j ;
+                for(j = 0 ; j < l->sz ; ++j) {
+                    int v = l->neighbours[j] ;
+                    --indeg[v] ;
+                }
+                break ; 
+            }
+        }
+        if(!found) break ; 
+    }
+    return res ;
+}
 int main() {
 	EdgeList *e = createEdgeList(FNAME);
 	AdjList adj[MAX_VERTICES];
@@ -106,6 +199,7 @@ int main() {
 	for(i=0;i<e->sz;i++) {
 		outdeg[(e->arr)[i].u]++;
 		indeg[(e->arr)[i].v]++;
-	}				
+    }
+    // too lazy to test anything 
 	return 0;
 }
